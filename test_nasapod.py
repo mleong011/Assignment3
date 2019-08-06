c8fe17bdb36f2c7794c468470b5c36632b3a142e
Mary Leong <mleong011@gmail.com>
2019-08-05 22:22:53 -0400

    add

--- test_nasapod.py
+++ test_nasapod.py
@@ -10,9 +10,6 @@ class HomeViewTest(unittest.TestCase):
     	self.app = nasapod.app.test_client()
     	self.app.testing = True
 
-    def test_home_page(self):
-        home = self.app.get('/')
-        self.assertIn('Home Page', str(home.data))
 
 
 if __name__ == "__main__":
